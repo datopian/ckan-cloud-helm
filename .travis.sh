a46e00f32386213fb717d1c50112d7774ead7361
Adam Kariv <adam.kariv@gmail.com>
2019-11-28 18:33:45 +0200

    cont

--- .travis.sh
+++ .travis.sh
@@ -12,9 +12,9 @@ elif [ "${1}" == "script" ]; then
     exit 0
 
 elif [ "${1}" == "deploy" ]; then
-    travis_ci_operator.sh github-update self ${TRAVIS_BRANCH} "
+    travis_ci_operator.sh github-update self master "
         cd charts_repository &&\
-        git fetch --all && git checkout origin/master -- index.yaml && git commit -m. &&\
+        git checkout ${TRAVIS_BRANCH} && git checkout master -- index.yaml && git commit -m. &&\
         helm package ../ckan --version "${TRAVIS_TAG}" &&\
         helm package ../efs --version "${TRAVIS_TAG}" &&\
         helm package ../traefik --version "${TRAVIS_TAG}" &&\
