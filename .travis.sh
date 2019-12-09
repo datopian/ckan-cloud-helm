d42d2729ddcc755760709ecbeede8e82ddc1e55e
Adam Kariv <adam.kariv@gmail.com>
2019-12-09 22:49:10 +0200

    Fix deploy to master

--- .travis.sh
+++ .travis.sh
@@ -14,7 +14,7 @@ elif [ "${1}" == "script" ]; then
 elif [ "${1}" == "deploy" ]; then
     travis_ci_operator.sh github-update self master "
         cd charts_repository &&\
-        git checkout ${TRAVIS_BRANCH} && git checkout master -- index.yaml && git commit -m. &&\
+        git checkout ${TRAVIS_BRANCH} && git checkout master -- index.yaml && git commit -m. ;\
         helm package ../ckan --version "${TRAVIS_TAG}" &&\
         helm package ../efs --version "${TRAVIS_TAG}" &&\
         helm package ../traefik --version "${TRAVIS_TAG}" &&\
