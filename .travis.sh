0fa32ee3f371202eb1c7adcbe66127400aab7ec2
ismailshahzad <ismailshahzadmirza@gmail.com>
2020-12-22 20:45:40 +0500

    [.travis.sh][s]: Adds path and sudo
    - Adds path to the travis_ci_operator.sh
    - Adds sudo before helm commands

--- .travis.sh
+++ .travis.sh
@@ -12,14 +12,14 @@ elif [ "${1}" == "script" ]; then
     exit 0
 
 elif [ "${1}" == "deploy" ]; then
-    travis_ci_operator.sh github-update self master "
+    $HOME/bin/travis_ci_operator.sh github-update self master "
         cd charts_repository &&\
         git checkout ${TRAVIS_BRANCH} && git checkout master -- index.yaml && git commit -m. ;\
-        helm package ../ckan --version "${TRAVIS_TAG}" &&\
-        helm package ../efs --version "${TRAVIS_TAG}" &&\
-        helm package ../traefik --version "${TRAVIS_TAG}" &&\
-        helm package ../provisioning --version "${TRAVIS_TAG}" &&\
-        helm repo index --url https://raw.githubusercontent.com/ViderumGlobal/ckan-cloud-helm/master/charts_repository/ . &&\
+        sudo helm package ../ckan --version "${TRAVIS_TAG}" &&\
+        sudo helm package ../efs --version "${TRAVIS_TAG}" &&\
+        sudo helm package ../traefik --version "${TRAVIS_TAG}" &&\
+        sudo helm package ../provisioning --version "${TRAVIS_TAG}" &&\
+        sudo helm repo index --url https://raw.githubusercontent.com/ViderumGlobal/ckan-cloud-helm/master/charts_repository/ . &&\
         cd .. &&\
         git stash &&\
         git checkout master &&\
