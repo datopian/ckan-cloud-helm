b9edcde9c17e679fa1f523adbffd1d4482ab8bb8
Your Name <you@example.com>
2021-04-01 15:17:20 +0400

    remove echos

--- travis_ci_operator.sh
+++ travis_ci_operator.sh
@@ -9,7 +9,6 @@ _install_script() {
     if [ -e "${1}" ]; then cp "${1}" "${GITHUB_WORKSPACE}/bin/${1}"
     else curl -L "https://raw.githubusercontent.com/OriHoch/travis-ci-operator/master/${1}" > "${GITHUB_WORKSPACE}/bin/${1}"
     fi && chmod +x "${GITHUB_WORKSPACE}/bin/${1}"
-    echo "++++++++++++++++++++++++++++"
     ls -alh "${GITHUB_WORKSPACE}/bin/${1}"
 }
 
@@ -43,7 +42,6 @@ elif [ "${1}" == "github-update" ]; then
     [ "${DEPLOY_KEY_NAME}" == "self" ] && [ "${COMMIT_MSG}" == "${TRAVIS_COMMIT_MESSAGE}" ] && [ "${GIT_BRANCH}" == "${TRAVIS_BRANCH}" ] \
         && echo skipping update of self with same commit msg and branch && exit 0
     [ -z "${GITHUB_REPO_SLUG}" ] && echo missing GITHUB_REPO_SLUG && exit 1
-    echo "============================="
     ls -alh /home/runner/bin/read_yaml.py
     ! $(eval echo `python ${GITHUB_WORKSPACE}/bin/read_yaml.py ${GITHUB_WORKSPACE}/bin/.travis-ci-operator.yaml ${DEPLOY_KEY_NAME}DeployKeyDecryptCmd`) \
         && echo Failed to get deploy key && exit 1
